868bf1de4aeb4a4f17b66087a257c812c1a579c5
Piotr255 <piotr.albinski1@gmail.com>
2023-10-18 12:58:45 +0200

    Lab1 do punktu 10

---
+++ oolab/src/main/java/agh/ics/oop/World.java
@@ -0,0 +1,16 @@
+package agh.ics.oop;
+
+public class World {
+
+    public static void run(){
+        System.out.println("Zwierzak idzie do przodu");
+
+    }
+    public static void main(String[] args){
+        System.out.println("system wystartował");
+        run();
+        System.out.println("system zakończył działanie");
+    }
+}
+
+
