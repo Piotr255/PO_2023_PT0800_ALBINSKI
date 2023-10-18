38a73e8b69c66f2bb7335b4f82f86ff00af279fc
Piotr255 <piotr.albinski1@gmail.com>
2023-10-18 13:49:07 +0200

    Lab1 do punktu 15

--- oolab/src/main/java/agh/ics/oop/World.java
+++ oolab/src/main/java/agh/ics/oop/World.java
@@ -3,22 +3,19 @@ package agh.ics.oop;
 public class World {
 
     public static void run(String[] params){
-
-        System.out.println("Zwierzak idzie do przodu");
-        int n = params.length;
-        for(int i = 0 ; i < n ; i+=1 ){
-            if (i != n-1){
-                System.out.print(params[i] + ",");
-            }
-            else {
-                System.out.println(params[i]);
+        for (String param : params) {
+            switch (param) {
+                case "f" -> System.out.println("Zwierzak idzie do przodu");
+                case "b" -> System.out.println("Zwierzak idzie do tyłu");
+                case "r" -> System.out.println("Zwierzak idzie w prawo");
+                case "l" -> System.out.println("Zwierzak idzie w lewo");
             }
         }
     }
     public static void main(String[] args){
-        System.out.println("system wystartował");
+        System.out.println("start");
         run(args);
-        System.out.println("system zakończył działanie");
+        System.out.println("stop");
     }
 }
 
