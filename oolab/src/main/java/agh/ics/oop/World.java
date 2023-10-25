e9d9ddb93faacf6f5b3bd710f3b4e1b403298037
Piotr255 <piotr.albinski1@gmail.com>
2023-10-25 20:13:59 +0200

    Dodane MapDirection oraz Vector2d

---
+++ oolab/src/main/java/agh/ics/oop/World.java
@@ -0,0 +1,34 @@
+
+package agh.ics.oop;
+
+import agh.ics.oop.model.MapDirection;
+import agh.ics.oop.model.MoveDirection;
+import agh.ics.oop.model.Vector2d;
+
+
+public class World {
+
+    public static void run(MoveDirection[] params){
+        for (MoveDirection param : params) {
+            switch (param) {
+                case FORWARD -> System.out.println("Zwierzak idzie do przodu");
+                case BACKWARD -> System.out.println("Zwierzak idzie do tyłu");
+                case RIGHT -> System.out.println("Zwierzak idzie w prawo");
+                case LEFT -> System.out.println("Zwierzak idzie w lewo");
+            }
+        }
+    }
+    public static void main(String[] args){
+        Vector2d position1 = new Vector2d(1,2);
+        System.out.println(position1);
+        Vector2d position2 = new Vector2d(-2,1);
+        System.out.println(position2);
+        System.out.println(position1.add(position2));
+
+        System.out.println(MapDirection.SOUTH);
+        System.out.println(MapDirection.NORTH.next());
+        System.out.println(MapDirection.NORTH.previous());
+        System.out.println(MapDirection.EAST.toUnitVector());
+    }
+}
+
---
+++ oolab/src/main/java/agh/ics/oop/model/MoveDirection.java
@@ -0,0 +1,10 @@
+
+
+package agh.ics.oop.model;
+
+public enum MoveDirection {
+    FORWARD,
+    BACKWARD,
+    RIGHT,
+    LEFT
+}
