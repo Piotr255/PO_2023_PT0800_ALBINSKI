ea9605fbdfe9778e9f016a5a2fa76408a0612c42
Piotr255 <146641708+Piotr255@users.noreply.github.com>
2023-12-20 13:12:33 +0100

    początek Przyciski i interakcje

--- oolab/src/main/java/agh/ics/oop/SimulationApp.java
+++ oolab/src/main/java/agh/ics/oop/SimulationApp.java
@@ -1,11 +1,17 @@
 package agh.ics.oop;
 
+import agh.ics.oop.model.ConsoleMapDisplay;
+import agh.ics.oop.model.MoveDirection;
+import agh.ics.oop.model.RectangularMap;
+import agh.ics.oop.model.Vector2d;
 import agh.ics.oop.presenter.SimulationPresenter;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+
+import java.util.List;
 
 public class SimulationApp extends Application {
     private void configureStage(Stage primaryStage, BorderPane viewRoot) {
@@ -15,6 +21,10 @@ public class SimulationApp extends Application {
         primaryStage.minWidthProperty().bind(viewRoot.minWidthProperty());
         primaryStage.minHeightProperty().bind(viewRoot.minHeightProperty());
     }
+
+
+
+
     @Override
     public void start(Stage primaryStage) throws Exception {
         primaryStage.show();
@@ -23,5 +33,11 @@ public class SimulationApp extends Application {
         BorderPane viewRoot = loader.load();
         SimulationPresenter presenter = loader.getController();
         configureStage(primaryStage,viewRoot);
+        RectangularMap simulationMap = new RectangularMap(5, 5);
+        simulationMap.subscribe(presenter);
+        presenter.setWorldMap(simulationMap);
+        List<MoveDirection> directions = OptionsParser.convert(new String[]{"l","l","r","f","f","f","f", "l", "r", "b"});
+        List<Vector2d> positions = List.of(new Vector2d(2,2),new Vector2d(4,4));
+        Simulation simulation = new Simulation(positions, directions,simulationMap);
     }
 }
--- oolab/src/main/java/agh/ics/oop/presenter/SimulationPresenter.java
+++ oolab/src/main/java/agh/ics/oop/presenter/SimulationPresenter.java
@@ -1,11 +1,32 @@
 package agh.ics.oop.presenter;
 
+import agh.ics.oop.model.MapChangeListener;
 import agh.ics.oop.model.WorldMap;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
-public class SimulationPresenter {
+public class SimulationPresenter implements MapChangeListener {
     WorldMap worldMap;
+    @FXML
+    private Label infoLabel;
 
+    @FXML
+    private TextField textField;
+
+    private void drawMap(){
+        infoLabel.setText(worldMap.toString());
+    }
     public void setWorldMap(WorldMap worldMap) {
         this.worldMap = worldMap;
+    }
+
+    @Override
+    public void mapChanged(WorldMap worldMap, String message) {
+        drawMap();
+    }
+
+    public void onSimulationStartClicked(ActionEvent actionEvent) {
     }
 }
